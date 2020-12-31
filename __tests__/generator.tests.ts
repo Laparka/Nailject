943a429bc1a47b2a014fb65afa7a8afe6bbac17c
Marat Sultangareev <msultangareev@accesssoftek.com>
2020-12-30 20:56:16 -0800

    Update generator.tests.ts

--- __tests__/generator.tests.ts
+++ __tests__/generator.tests.ts
@@ -2,6 +2,6 @@ import ServiceResolversGenerator from '../generator/serviceResolversGenerator';
 
 test("Must Generate Module Service Resolvers", () => {
   const generator = new ServiceResolversGenerator();
-  const resolvers = generator.generate('./__tests__/module.ts', 'Module');
+  const resolvers = generator.parse('./__tests__/module.ts', 'Module');
   console.log(resolvers);
 });
