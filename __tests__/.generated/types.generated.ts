aecf7ed9fca9b6d3c77b90e54dd333cdf3a91b8f
Marat Sultangareev <msultangareev@accesssoftek.com>
2021-01-03 22:49:22 -0800

    Deleted the generated files

--- __tests__/.generated/types.generated.ts
+++
@@ -1,8 +0,0 @@
-export const __tests__interfaces = {
-    Logger: Symbol.for('Logger'),
-    MonitorOfLogger: Symbol.for('MonitorOfLogger'),
-    MonitorOfstring: Symbol.for('MonitorOfstring'),
-    MonitorOfany: Symbol.for('MonitorOfany'),
-    TwoGenericsOfMonitorOfLoggerOfConsoleMonitorOfConsoleLogger: Symbol.for('TwoGenericsOfMonitorOfLoggerOfConsoleMonitorOfConsoleLogger'),
-};
-
